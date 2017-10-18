6c1863843de863100ffc6936501f8dcd67dab2e3
Jonathan Clem <jonathan@jclem.net>
2017-10-18 11:17:54 -0400

    zsh: Add local zsh-functions

--- zsh/fpath.zsh
+++ zsh/fpath.zsh
@@ -3,3 +3,8 @@ autoload -U $ZSH_CONFIG_DIR/completions/*(:t)
 
 fpath=($ZSH_CONFIG_DIR/functions $fpath)
 autoload -U $ZSH_CONFIG_DIR/functions/*(:t)
+
+if [ -d $HOME/.config/zsh-functions ]; then
+  fpath=($HOME/.config/zsh-functions $fpath)
+  autoload -U $HOME/.config/zsh-functions/*(:t)
+fi
