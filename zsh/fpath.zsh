104e20819ddc9276b0fdb2d0456d3da08778e73c
Jonathan Clem <jonathan@jclem.net>
2017-10-18 11:17:58 -0400

    zsh: Safer check for ~/.config/zsh-functions

--- zsh/fpath.zsh
+++ zsh/fpath.zsh
@@ -4,7 +4,7 @@ autoload -U $ZSH_CONFIG_DIR/completions/*(:t)
 fpath=($ZSH_CONFIG_DIR/functions $fpath)
 autoload -U $ZSH_CONFIG_DIR/functions/*(:t)
 
-if [ -d $HOME/.config/zsh-functions ]; then
+if [[ -d $HOME/.config/zsh-functions && -n "$(ls -A $HOME/.config/zsh-functions)" ]]; then
   fpath=($HOME/.config/zsh-functions $fpath)
   autoload -U $HOME/.config/zsh-functions/*(:t)
 fi
